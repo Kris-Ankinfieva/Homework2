4c5021897736bca7dedb248efa66aa0d69f3b97e
Kris Ankinfieva <kk397758@yandex.ru>
2022-10-11 00:24:53 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа. 
+// Не использовать строки для расчета.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// // array[] = {intIndex[0], intIndex[1], intIndex[2]};
+// int result = intIndex[1];
+// Console.WriteLine(result);
+
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int index1(int n)
+{
+    int result = (n % 100) / 10;
+    if (n > 99 & n < 1000) Console.WriteLine($"{result} - второе значение числа");
+
+else 
+{
+    if (n > -1000 & n < -99) Console.WriteLine($"{result * -1} второе значение числа");
+    else
+    {
+        Console.WriteLine($"Число {n} не трёхзначное");
+    }
+}   
+return n;
+}
+
+int result = index1(n);
