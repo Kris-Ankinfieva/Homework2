3818c168cf7af68e39b822a04fb829240e86ef30
Kris Ankinfieva <kk397758@yandex.ru>
2022-10-11 00:54:55 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет. 
+// Не использовать строки для расчета.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int PrintNumber(int msg)
+{
+    if (msg > 99) Console.Write($"-> {msg % 10}");
+    else
+    {
+        if (msg < -99) Console.Write($"-> {(msg % 10) * -1}");
+        else
+        {
+            Console.Write("-> число не является трёхзначным");
+        }
+    }
+    return msg;
+}
+int DevisionNumber(int N)
+{
+    while (N > 1000 || N < -1000)
+    {
+        N /= 10;
+    }
+    Console.Write($"{number} ");
+    return N;
+}
+
+int max = (PrintNumber(DevisionNumber(number)));
