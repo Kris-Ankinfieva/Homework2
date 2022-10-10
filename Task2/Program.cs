0a3304822ef5ccb70499ec151be4741c38da3b66
Kris Ankinfieva <kk397758@yandex.ru>
2022-10-11 00:54:47 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа. 
+// Не использовать строки для расчета.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int random = new Random().Next(-999, 1000);
+
+int FilNumber(int number)
+{
+    Console.Write($"Случайное трёхзначное число: {random} ");
+    return number;
+}
+int DelNumber(int n)
+{
+    int n1 = (n % 1000) / 100;
+    int n2 = random % 10;
+    if (n > 100 & n < 1000) System.Console.Write($"-> {n1}{n2}");
+    else
+    {
+    if (n > -999 & n < -99) System.Console.Write($"-> {n1}{n2 * -1}");
+    else
+    {
+        Console.Write("- число не является трёхзначным");
+    }
+    }
+    return n;
+}
+int result = (DelNumber(FilNumber(random)));
